b8c64e017237ceb76da271e95d05f65239a75dc7
SurajDadral <dadralj18@gmail.com>
2019-09-29 20:02:11 +0530

    Add shell script to setup crontab

---
+++ setup.sh
@@ -0,0 +1,21 @@
+# Install required packages
+sudo apt install python3 python3-opencv cron sed -y
+
+# Start and enable crontab to schedule jobs at system boot
+sudo systemctl start cron
+sudo systemctl enable cron
+
+# Create directories to store captured images and logs
+sudo mkdir -p /var/TrackSystemLogin/captures /var/TrackSystemLogin/logs
+
+# Copy source code to /opt/TrackSystemLogin/ directory
+sudo mkdir -p /opt/TrackSystemLogin/
+sudo cp -r $(dirname "$0")/* /opt/TrackSystemLogin/
+
+# Setup crontab job to execute script at startup
+sudo mkdir -p /var/spool/cron/crontabs/
+sudo touch /var/spool/cron/crontabs/root
+if ! grep -Fxq "" /var/spool/cron/crontabs/root
+then
+	sudo echo "@reboot python3 /opt/TrackSystemLogin/capture_image.py" >> /var/spool/cron/crontabs/root
+fi
