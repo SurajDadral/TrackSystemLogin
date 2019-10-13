cf2d15f9a59ce7f58dcfdbdebfc7e0edbab549aa
SurajDadral <dadralj18@gmail.com>
2019-10-13 19:21:43 +0530

    Solve error mkdir: missing operand

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ sudo systemctl start cron
 sudo systemctl enable cron
 
 # Get image and logs storage directory from config.py file
-IMAGE_DIR=$(cat config.py | egrep "config\['IMAGE_DIR'\]" | sed -e "s/^\(.*\)= //g")
-LOGS_DIR=$(cat config.py | egrep "config\['LOGS_DIR'\]" | sed -e "s/^\(.*\)= //g")
+IMAGE_DIR=$(cat config.py | egrep "config\[\"IMAGE_DIR\"\]" | sed -e "s/^\(.*\)= //g")
+LOGS_DIR=$(cat config.py | egrep "config\[\"LOGS_DIR\"\]" | sed -e "s/^\(.*\)= //g")
 
 # Create directories to store captured images and logs
 sudo mkdir -p $IMAGE_DIR $LOGS_DIR
