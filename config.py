2f84656664dbf973a383d42f4c2da576c2421508
SurajDadral <dadralj18@gmail.com>
2019-10-13 19:02:00 +0530

    Add git integration for image storage

--- config.py
+++ config.py
@@ -3,33 +3,38 @@ from datetime import datetime
 config = {}
 
 # Path of image storage directory
-config['IMAGE_DIR'] = "/var/TrackSystemLogin/captures/"
+config["IMAGE_DIR"] = "/var/TrackSystemLogin/captures/"
 
 # Path of log files
-config['LOGS_DIR'] = "/var/TrackSystemLogin/logs/"
+config["LOGS_DIR"] = "/var/TrackSystemLogin/logs/"
+
+# Send email
+config["SEND_EMAIL"] = True
 
 # SMTP server address
-config['SMTP_SERVER_ADDRESS'] = "smtp.gmail.com"
+config["SMTP_SERVER_ADDRESS"] = "smtp.gmail.com"
 
 # SMTP server port
-config['SMTP_SSL_PORT'] = 465
+config["SMTP_SSL_PORT"] = 465
 
 # Email address used to send emails
-config['FROM_EMAIL_ADDRESS'] = ""
+config["FROM_EMAIL_ADDRESS"] = ""
 
 # Email address used to recieve emails on each login
-config['TO_EMAIL_ADDRESS'] = ""
+config["TO_EMAIL_ADDRESS"] = ""
 
 # Password of FROM_EMAIL_ADDRESS
-config['PASSWORD_OF_FROM_EMAIL_ADDRESS'] = ""
+config["PASSWORD_OF_FROM_EMAIL_ADDRESS"] = ""
 
 # Subject of email
-config['EMAIL_SUBJECT'] = "System blank@noip powered on at " + str(
+config["EMAIL_SUBJECT"] = "System blank@noip powered on at " + str(
     datetime.now().strftime("%b %d, %Y %H:%M:%S")
 )
 
 # Body of email
-config['EMAIL_BODY'] = """\
+config[
+    "EMAIL_BODY"
+] = """\
 Hi Sir,
 Someone powered on your laptop at %s.
 Please check images of person, who powered on your system, in attachment.
@@ -39,5 +44,22 @@ Consider taking appropriate action.
 Thank you.
 Have a nice day.
 """ % str(
+    datetime.now().strftime("%b %d, %Y %H:%M:%S")
+)
+
+# Git commit
+config["GIT_COMMIT"] = False
+
+# Github username or API token
+config["GITHUB_USERNAME_OR_API_TOKEN"] = ""
+
+# Github user password
+config["GITHUB_USER_PASSWORD"] = ""
+
+# Github repository name
+config["GITHUB_REPOSITORY"] = "TrackSystemLoginImageStorage"
+
+# Git commit message
+config["GIT_COMMIT_MESSAGE"] = "System blank@noip powered on at " + str(
     datetime.now().strftime("%b %d, %Y %H:%M:%S")
 )
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 # Install required packages
 sudo apt install python3 python3-opencv cron sed -y
+sudo python3 -m pip install PyGithub
 
 # Start and enable crontab to schedule jobs at system boot
 sudo systemctl start cron
@@ -19,8 +20,8 @@ sudo cp -r $(dirname "$0")/* /opt/TrackSystemLogin/
 # Setup crontab job to execute script at startup
 sudo mkdir -p /var/spool/cron/crontabs/
 sudo touch /var/spool/cron/crontabs/root
-if ! grep -Fxq "@reboot python3 /opt/TrackSystemLogin/main.py" /var/spool/cron/crontabs/root
+if ! sudo grep -Fxq "@reboot python3 /opt/TrackSystemLogin/main.py" /var/spool/cron/crontabs/root
 then
-	sudo echo "@reboot python3 /opt/TrackSystemLogin/main.py" >> /var/spool/cron/crontabs/root
+	sudo bash -c 'echo "@reboot python3 /opt/TrackSystemLogin/main.py" >> /var/spool/cron/crontabs/root'
 fi
 sudo crontab /var/spool/cron/crontabs/root
